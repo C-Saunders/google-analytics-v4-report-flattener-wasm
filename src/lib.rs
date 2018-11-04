ee45a3f909d5ddbe929ed116bdede7b85b551be2
Charlie Saunders <charlieasaunders@gmail.com>
2018-11-04 09:52:52 -0500

    Updates output types and adds functions that accept pre-parsed data.
    * Updates output types to be wasm-bindgen::JsValues, rather than strings
    * Adds parsedToDelimited and parsedtoFlatJson functions which accept a
    parsed report response as their input, rather than a string
    * Updates ga-v4-flattener dependency version to enable this

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+extern crate console_error_panic_hook;
 extern crate ga_v4_flattener;
 extern crate serde;
 extern crate serde_json;
 extern crate wasm_bindgen;
-extern crate console_error_panic_hook;
 
+use ga_v4_flattener::to_delimited::response_to_delimited_reports;
+use ga_v4_flattener::to_row_array::response_to_row_array;
+use ga_v4_flattener::types::ReportResponse;
 use ga_v4_flattener::{to_delimited, to_flat_json};
 use wasm_bindgen::prelude::*;
 
@@ -12,17 +15,35 @@ fn set_panic_hook() {
 }
 
 #[wasm_bindgen]
-#[allow(non_snake_case)] 
-pub fn toDelimited(data: &str, delimiter: &str) -> String {
+#[allow(non_snake_case)]
+pub fn toDelimited(data: &str, delimiter: &str) -> JsValue {
     set_panic_hook();
 
-    serde_json::to_string(&to_delimited(data, delimiter).unwrap()).unwrap()
+    JsValue::from_serde(&to_delimited(data, delimiter).unwrap()).unwrap()
 }
 
 #[wasm_bindgen]
-#[allow(non_snake_case)] 
-pub fn toFlatJson(data: &str) -> String {
+#[allow(non_snake_case)]
+pub fn parsedToDelimited(param_data: &JsValue, delimiter: &str) -> JsValue {
     set_panic_hook();
-    
-    serde_json::to_string(&to_flat_json(data).unwrap()).unwrap()
+
+    let data: ReportResponse = param_data.into_serde().unwrap();
+    JsValue::from_serde(&response_to_delimited_reports(&data, delimiter)).unwrap()
+}
+
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn toFlatJson(data: &str) -> JsValue {
+    set_panic_hook();
+
+    JsValue::from_serde(&to_flat_json(data).unwrap()).unwrap()
+}
+
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+pub fn parsedtoFlatJson(param_data: &JsValue) -> JsValue {
+    set_panic_hook();
+
+    let data: ReportResponse = param_data.into_serde().unwrap();
+    JsValue::from_serde(&response_to_row_array(&data)).unwrap()
 }
